40cee5c5825793dc8415362a9f6127b650797a66
Alastair <alastair@nvision-imaging.com>
2020-09-08 11:09:40 +0200

    create some pulses in the test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,12 @@ using Test
 
 @testset "NMRPulses.jl" begin
     # Write your tests here.
+    id = IdlePulse(1, 1e-3)
+    Ω = 2*pi
+    pi_2 = PiHalfPulse(Ω, 0, 1e-3)
+    pi_pulse = PiPulse(Ω, 0, 1e-3)
+
+    s = Sequence([pi_2, id, pi_pulse, id, pi_2])
+
+    a, b, c = prepare_sequence(s)
 end
